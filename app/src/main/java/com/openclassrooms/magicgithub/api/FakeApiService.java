b7f5d70796ba655b18e1ed0d96039475a0821628
Monokouma <aurange.thomas@gmail.com>
2022-04-06 17:00:33 +0200

    first commit

--- app/src/main/java/com/openclassrooms/magicgithub/api/FakeApiService.java
+++ app/src/main/java/com/openclassrooms/magicgithub/api/FakeApiService.java
@@ -2,9 +2,6 @@ package com.openclassrooms.magicgithub.api;
 
 import com.openclassrooms.magicgithub.model.User;
 import java.util.List;
-import java.util.Random;
-
-import static com.openclassrooms.magicgithub.api.FakeApiServiceGenerator.FAKE_USERS_RANDOM;
 import static com.openclassrooms.magicgithub.api.FakeApiServiceGenerator.generateUsers;
 
 public class FakeApiService implements ApiService {
@@ -18,7 +15,7 @@ public class FakeApiService implements ApiService {
     @Override
     public List<User> getUsers() {
         // TODO A modifier
-        return users;
+        return null;
     }
 
     /**
@@ -28,7 +25,6 @@ public class FakeApiService implements ApiService {
     @Override
     public void generateRandomUser() {
         // TODO A modifier
-        users.add(FAKE_USERS_RANDOM.get(new Random().nextInt(FAKE_USERS_RANDOM.size())));
     }
 
     /**
@@ -37,6 +33,5 @@ public class FakeApiService implements ApiService {
     @Override
     public void deleteUser(User user) {
         // TODO A modifier
-        users.remove(user);
     }
 }
